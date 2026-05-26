065fba65f0b9f1ea98bb1a258b63a98a2189215c
danlevimb <danlevimb@gmail.com>
2026-05-26 14:52:23 -0600

    Environment update

--- notebooks/00_environment_setup.py
+++ notebooks/00_environment_setup.py
@@ -1,3 +1,6 @@
+# Databricks notebook source
+# #%%
+
 storage_account = "stdanadblh4827"
 
 landing_base_path = "abfss://landing@" + storage_account + ".dfs.core.windows.net"
@@ -12,3 +15,8 @@ print("Bronze: " + bronze_base_path)
 print("Silver: " + silver_base_path)
 print("Gold: " + gold_base_path)
 print("Metadata: " + metadata_base_path)
+
+# %%
+# COMMAND ----------
+
+display(dbutils.fs.ls(landing_base_path + "/source_data"))
